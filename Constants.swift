034f2f43ef0895063331b40e7288618f269ed90c
Alex Beattie <alexanderbeattie@mac.com>
2015-11-06 08:46:57 -0800

    Initial Commit

---
+++ Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  showcase-dev
+//
+//  Created by Alex Beattie on 11/6/15.
+//  Copyright © 2015 Alex Beattie. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0

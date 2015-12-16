f9d820f344f83a4c5022f21eac6cb3a11cc31ba1
Alex Beattie <alexanderbeattie@mac.com>
2015-12-16 10:41:14 -0800

    Fixed App Transport for Images to download. Added Image Picker Upload

--- FeedVC.swift
+++ FeedVC.swift
@@ -9,19 +9,29 @@
 import UIKit
 import Firebase
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
     @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var postField: MaterialTextField!
+    @IBOutlet weak var imageSelectorImage: UIImageView!
     var posts = [Post]()
+    static var imageCache = NSCache()
+    
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.estimatedRowHeight = 358
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self 
+        
         
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
+
             print(snapshot.value)
             
             self.posts = []
@@ -31,40 +41,65 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                 for snap in snapshots {
                     print("SNAP: \(snap)")
                     
-                    
                     if let postDict = snap.value as? Dictionary<String, AnyObject> {
                         let key = snap.key
                         let post = Post(postKey: key, dictionary: postDict)
                         self.posts.append(post)
                     }
-                    
                 }
-              
             }
-            
-            
             self.tableView.reloadData()
         })
         // Do any additional setup after loading the view.
     }
 
-   
-
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        
         return 1
     }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        
+     
+        
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell {
+            cell.request?.cancel()
+
+            var img: UIImage?
+
+            if let url = post.imageUrl {
+                img = FeedVC.imageCache.objectForKey(url) as? UIImage
+            }
+            
+            cell.configureCell(post, img: img)
+            
+            return cell
+        } else {
+            return PostCell()
+        }
+    }
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let post = posts[indexPath.row]
+        if post.imageUrl == nil {
+            return 200
+        } else {
+            return tableView.estimatedRowHeight
+        }
+    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        imageSelectorImage.image = image
+    }
+    
+    @IBAction func selectImage(sender: UITapGestureRecognizer) {
+        presentViewController(imagePicker, animated: true, completion: nil)
         
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let post = posts[indexPath.row]
-        print(post.postDescription)
-        
-        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    @IBAction func makePost(sender: AnyObject) {
     }
-
 }
